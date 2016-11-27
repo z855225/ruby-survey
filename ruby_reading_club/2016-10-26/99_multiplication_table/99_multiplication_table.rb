f5950be177f5cba6d67c8cc8b464b87fa0b4cd9b
River <z855225@gmail.com>
2016-11-27 21:48:15 +0800

    修正縮排

--- ruby_reading_club/2016-10-26/99_multiplication_table/99_multiplication_table.rb
+++ ruby_reading_club/2016-10-26/99_multiplication_table/99_multiplication_table.rb
@@ -2,11 +2,11 @@ a = 2
 b = 1
 
 while a <= 9
-		while b <= 9
-				print "#{a} X #{b} = "
-				puts a * b
-				b+=1
-		end
-		b=1
-		a+=1	
+	while b <= 9
+		print "#{a} X #{b} = "
+		puts a * b
+		b+=1
+	end
+	b=1
+	a+=1	
 end
--- ruby_reading_club/2016-11-02/advanced_2020_multiplication_table/advanced_2020_multiplication_table.rb
+++ ruby_reading_club/2016-11-02/advanced_2020_multiplication_table/advanced_2020_multiplication_table.rb
@@ -1,7 +1,7 @@
 require 'Prime'
 
 for x in 2..20
-		for y in 1..20
-    		puts "#{x} * #{y} = #{x * y}" unless y.prime?
-		end
+	for y in 1..20
+		puts "#{x} * #{y} = #{x * y}" unless y.prime?
+	end
 end
